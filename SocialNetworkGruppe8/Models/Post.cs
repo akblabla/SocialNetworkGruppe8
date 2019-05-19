72c894753db5dd49fe955c8ec7953a4cc42e1381
grottrup <grottrup@protonmail.com>
2019-05-19 16:38:01 +0200

    Add post types

--- SocialNetworkGruppe8/Models/Post.cs
+++ SocialNetworkGruppe8/Models/Post.cs
@@ -6,13 +6,13 @@ using MongoDB.Bson.Serialization.Attributes;
 using SocialNetworkGruppe8.Models;
 namespace SocialNetworkGruppe8.Models
 {
-    public class Post
+    public abstract class Post
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string UserId { get; set; }
-        public string Text { get; set; }
+        public string Author { get; set; }
         public bool IsPublic { get; set; }
         public DateTime CreationDate { get; set; }
     }
